9f4f82775c1779339f02517ec5df01b9f4327ff9
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2024-08-14 16:55:01 +0300

    Partially implemented bookService and MemberService

--- src/main/java/com/testtask/nerdysoft/librarymanagement/service/impl/BookServiceImpl.java
+++ src/main/java/com/testtask/nerdysoft/librarymanagement/service/impl/BookServiceImpl.java
@@ -1,47 +1,84 @@
 package com.testtask.nerdysoft.librarymanagement.service.impl;
 
+import com.testtask.nerdysoft.librarymanagement.exception.BookNotFoundException;
 import com.testtask.nerdysoft.librarymanagement.model.Book;
+import com.testtask.nerdysoft.librarymanagement.repository.BookRepository;
 import com.testtask.nerdysoft.librarymanagement.service.BookService;
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class BookServiceImpl implements BookService {
 
-    private BookService bookService;
-    private Validator validator;
+    private final BookRepository bookRepository;
+    private final Validator validator;
 
     @Autowired
-    public BookServiceImpl(BookService bookService, Validator validator) {
-        this.bookService = bookService;
+    public BookServiceImpl(BookRepository bookRepository, Validator validator) {
+        this.bookRepository = bookRepository;
         this.validator = validator;
     }
 
     @Override
     public Book addBook(Book book) {
-        return null;
+        validateBook(book);
+        Optional<Book> existingBook = bookRepository.findByTitleAndAuthor(book.getTitle(), book.getAuthor());
+        if (existingBook.isPresent()) {
+            Book bookToUpdate = existingBook.get();
+            bookToUpdate.setAmount(bookToUpdate.getAmount() + 1);
+            return bookRepository.save(bookToUpdate);
+        } else {
+            return bookRepository.save(book);
+        }
     }
 
     @Override
     public Book getBookById(Long id) {
-        return null;
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException("Book not found with id: " + id));
     }
 
     @Override
     public List<Book> getAllBooks() {
-        return List.of();
+        return bookRepository.findAll();
     }
 
     @Override
     public Book updateBookById(Long id, Book updatedBook) {
-        return null;
+        Book bookToUpdate = getBookById(id);
+        bookToUpdate.setTitle(updatedBook.getTitle());
+        bookToUpdate.setAuthor(updatedBook.getAuthor());
+        bookToUpdate.setAmount(updatedBook.getAmount());
+        Optional<Book> existingBook = bookRepository.findByTitleAndAuthor(bookToUpdate.getTitle(), bookToUpdate.getAuthor());
+        // todo deletion of the entry while its id is being referenced somewhere
+        return bookRepository.save(bookToUpdate);
     }
 
     @Override
     public void deleteBook(Long id) {
+        Book book = getBookById(id);
+        // todo prevent deletion if book is borrowed
+        if (book.getAmount() == 0) {
+            bookRepository.delete(book);
+        } else {
+            throw new IllegalStateException("Cannot delete book that is currently borrowed.");
+        }
+    }
 
+    private void validateBook(Book book) {
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+
+        if (!violations.isEmpty()) {
+            List<String> errorMessages = violations.stream()
+                    .map(violation -> violation.getPropertyPath() + " " + violation.getMessage())
+                    .toList();
+            throw new IllegalArgumentException("Provided Book has errors: " + errorMessages);
+        }
     }
 }
--- src/main/java/com/testtask/nerdysoft/librarymanagement/service/impl/MemberServiceImpl.java
+++ src/main/java/com/testtask/nerdysoft/librarymanagement/service/impl/MemberServiceImpl.java
@@ -1,45 +1,79 @@
 package com.testtask.nerdysoft.librarymanagement.service.impl;
 
+import com.testtask.nerdysoft.librarymanagement.exception.MemberNotFoundException;
 import com.testtask.nerdysoft.librarymanagement.model.Member;
+import com.testtask.nerdysoft.librarymanagement.repository.MemberRepository;
 import com.testtask.nerdysoft.librarymanagement.service.MemberService;
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 
-    private MemberService memberService;
+    private final MemberRepository memberRepository;
+    private final Validator validator;
+
+    @Value("${library.maxBooksPerMember}")
+    private int maxBooksPerMember;
 
     @Autowired
-    public MemberServiceImpl(MemberService memberService, Validator validator) {
-        this.memberService = memberService;
+    public MemberServiceImpl(MemberRepository memberRepository, Validator validator) {
+        this.memberRepository = memberRepository;
+        this.validator = validator;
     }
 
     @Override
     public Member addMember(Member member) {
-        return null;
+        validateMember(member);
+
+        member.setMembershipDate(LocalDate.now());
+        return memberRepository.save(member);
     }
 
     @Override
     public Member getMemberById(Long id) {
-        return null;
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new MemberNotFoundException("Member not found with id: " + id));
     }
 
     @Override
     public List<Member> getAllMembers() {
-        return List.of();
+        return memberRepository.findAll();
     }
 
     @Override
     public Member updateMember(Long id, Member updatedMember) {
-        return null;
+        validateMember(updatedMember);
+        Member existingMember = getMemberById(id);
+        existingMember.setName(updatedMember.getName());
+        return memberRepository.save(existingMember);
     }
 
     @Override
     public void deleteMember(Long id) {
+        Member member = getMemberById(id);
+        if (member.getBorrows().stream().noneMatch(b -> b.getReturnDate() == null)) {
+            memberRepository.delete(member);
+        } else {
+            throw new IllegalStateException("Cannot delete Member with borrowed books.");
+        }
+    }
 
+    private void validateMember(Member member) {
+        Set<ConstraintViolation<Member>> violations = validator.validate(member);
+
+        if (!violations.isEmpty()) {
+            List<String> errorMessages = violations.stream()
+                    .map(violation -> violation.getPropertyPath() + " " + violation.getMessage())
+                    .toList();
+            throw new IllegalArgumentException("Provided Member has errors: " + errorMessages);
+        }
     }
 }
